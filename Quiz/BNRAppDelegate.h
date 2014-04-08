cb2269a9cea931c0ab8e7c04b0d16c65112ed8e8
Ray Mangum <raymangum@gmail.com>
2014-04-07 21:33:42 -0400

    Initial Commit

---
+++ Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by James Ray Mangum on 4/7/14.
+//  Copyright (c) 2014 Ray Mangum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by James Ray Mangum on 4/7/14.
+//  Copyright (c) 2014 Ray Mangum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
